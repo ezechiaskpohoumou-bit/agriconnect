11307fe907dc2e826f0055823ebe26cbd47d9ca3
ezechiaskpohoumou-bit <ezechiaskpohoumou@gmail.com>
2025-12-29 14:57:40 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        // Pour l'instant, on laisse passer pour vérifier que le serveur démarre
+        // Pour l'instant on simule une connexion pour que ton site s'affiche
+        req.auth = { userId: "123" }; 
         next();
     } catch (error) {
         res.status(401).json({ message: 'Requête non authentifiée' });
